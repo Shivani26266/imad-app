fc88d53f51651490232b97c7f7ce1ed56441f4e1
Shivani26266 <36534242+Shivani26266@users.noreply.github.com>
2018-02-21 09:56:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,18 @@
 console.log('Loaded!');
+var button = document.getElementById('counter');
+button.onclick = function(){
+    var request = new XMLHttpRequest();
+    //capture the response and store into an variable.
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getelementByID('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        
+    };
+    request.open('GET', 'http://shivanidwivedi26266.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
